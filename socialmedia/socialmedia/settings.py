34b5e5d9ba635c9e31cd781d9c39bb0a554edf97
achrafkassimi <achraf.kassimi.1995@gmail.com>
2025-08-02 17:14:13 +0100

    Configurer PostgreSQL dans Django

--- socialmedia/socialmedia/settings.py
+++ socialmedia/socialmedia/settings.py
@@ -73,12 +73,24 @@ WSGI_APPLICATION = 'socialmedia.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'socialdb',
+        'USER': 'testuser',
+        'PASSWORD': '123456',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
+
 
 
 # Password validation
