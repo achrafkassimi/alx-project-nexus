c904ad2b86b5d194f0955a43e2954f42582de39f
achrafkassimi <achraf.kassimi.1995@gmail.com>
2025-08-07 00:29:21 +0100

    Like & Comment

--- socialmedia/feed/models.py
+++ socialmedia/feed/models.py
@@ -5,15 +5,23 @@ class Post(models.Model):
     author = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
     content = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
+    liked_by = models.ManyToManyField(CustomUser, related_name='liked_posts', blank=True)
+
+    def total_likes(self):
+        return self.liked_by.count()
 
     def __str__(self):
         return f"{self.author.username}'s Post"
 
 class Comment(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments')
     author = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
-    post = models.ForeignKey(Post, on_delete=models.CASCADE)
     content = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Comment by {self.author.username} on {self.post.id}"
+
 
 class Like(models.Model):
     user = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
