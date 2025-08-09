da54035db9740983935f00501555883da835b57a
achrafkassimi <achraf.kassimi.1995@gmail.com>
2025-08-09 22:57:03 +0100

    add pptx

--- socialmedia/socialmedia/settings.py
+++ socialmedia/socialmedia/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-0fgx8n1ml#4*%d=ebnk=mc($6#ovkmi6*(s-z(_gu*kx%mi1n7'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = []
 
